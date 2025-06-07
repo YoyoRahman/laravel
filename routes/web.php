f6dfb1744920f0be5d2ee99da94652f1d8481543
YoyoRahman <yoyorahman.nm@gmail.com>
2025-06-08 01:15:14 +0330

    first test

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return  view('index');
+});
+
+Route::get('/user', function () {
+
+    $name = 'یاسر';
+    return  view('user', compact('name'));
+});
+
+
+Route::get('/about', [\App\Http\Controllers\PageController::class, 'about']);
